e45246e5a471e50f9a7ba2de9e802c3283dbee43
Marge <marge.consunji@gmail.com>
2022-06-23 19:21:11 +0800

    Add placeholder text

--- src/components/checkout/Wizard.js
+++ src/components/checkout/Wizard.js
@@ -1,5 +1,13 @@
+import { Col, Progress, Button } from 'reactstrap';
+
 const Wizard = () => {
-  return <div>Checkout Wizard component</div>;
+  return (
+    <Col className="bg-white col-md-7 px-4 py-5 rounded">
+      <h2>Buy a Pass</h2>
+      <hr />
+      <small className="text-uppercase mb-2 d-block">Step 1 of 4</small>
+    </Col>
+  );
 };
 
 export default Wizard;
