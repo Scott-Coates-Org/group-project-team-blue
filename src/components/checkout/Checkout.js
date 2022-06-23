3ce40338f43358da976eb11131cb7b75e9998419
Marge <marge.consunji@gmail.com>
2022-06-23 19:13:59 +0800

    Import Checkout components
    - Import Container and Row components from reactstrap
    - Import Wizard and Cart components

--- src/components/checkout/Checkout.js
+++ src/components/checkout/Checkout.js
@@ -1,5 +1,18 @@
+import { Container, Row } from 'reactstrap';
+import Wizard from './Wizard';
+import Cart from './Cart';
+
 const Checkout = () => {
-  return <div>Checkout route</div>;
+  return (
+    <div className="vh-100">
+      <Container className="pt-5 px-4">
+        <Row xs="1" md="2" className="g-2">
+          <Wizard />
+          <Cart />
+        </Row>
+      </Container>
+    </div>
+  );
 };
 
 export default Checkout;
