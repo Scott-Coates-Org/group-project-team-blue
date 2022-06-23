017767ad25ec7a31bfb1e0a646052bdc5838a72e
Marge <marge.consunji@gmail.com>
2022-06-23 19:21:01 +0800

    Add placeholder text

--- src/components/checkout/Cart.js
+++ src/components/checkout/Cart.js
@@ -1,5 +1,12 @@
+import { Col } from 'reactstrap';
+
 const Cart = () => {
-  return <div>Checkout Cart component</div>;
+  return (
+    <Col className="bg-white col-md-5 px-4 py-5 rounded ml-n2">
+      <h2>Your Cart</h2>
+      <p>Your cart is currently empty.</p>
+    </Col>
+  );
 };
 
 export default Cart;
