2e025f369dd120d906653615158fb8aca50a0956
Marge <marge.consunji@gmail.com>
2022-06-24 13:26:07 +0800

    Extract wizard steps into their own components
    - Steps contain dummy text for now

---
+++ src/components/checkout/steps/AddOns.js
@@ -0,0 +1,11 @@
+import WizardStep from '../WizardStep';
+
+const ProductSelect = () => {
+  return (
+    <WizardStep stepHeader="Select add-ons">
+      <p>List of add-ons goes here</p>
+    </WizardStep>
+  );
+};
+
+export default ProductSelect;
