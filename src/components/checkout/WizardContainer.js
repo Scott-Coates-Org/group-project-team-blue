028755cca7138b2d60a3af86ad779b24b68ba613
Marge <marge.consunji@gmail.com>
2022-06-24 13:27:01 +0800

    Import wizard step components
    - Clean up code a bit and import step components

--- src/components/checkout/WizardContainer.js
+++ src/components/checkout/WizardContainer.js
@@ -1,34 +1,11 @@
-import { useState } from 'react';
-import { Col, Progress, Button } from 'reactstrap';
-import { Wizard, useWizard } from 'react-use-wizard';
-
-const Step1 = () => {
-  const { nextStep, isFirstStep } = useWizard();
-  return (
-    <>
-      <h3>Step1</h3>
-      <p>test</p>
-      <div>
-        <Button color="primary" onClick={() => nextStep()}>
-          Continue
-        </Button>
-      </div>
-    </>
-  );
-};
-const Step2 = () => {
-  const { previousStep, nextStep } = useWizard();
-  return (
-    <>
-      <h3>Step2</h3>
-      <p>test</p>
-      <div>
-        <Button onClick={() => previousStep()}>Back</Button>
-        <Button onClick={() => nextStep()}>Continue</Button>
-      </div>
-    </>
-  );
-};
+import { Col } from 'reactstrap';
+import { Wizard } from 'react-use-wizard';
+import WizardHeader from './WizardHeader';
+import WizardButtons from './WizardButtons';
+import ProductSelect from './steps/ProductSelect';
+import AddOns from './steps/AddOns';
+import Details from './steps/Details';
+import Purchase from './steps/Purchase';
 
 const WizardContainer = () => {
   return (
@@ -36,11 +13,11 @@ const WizardContainer = () => {
       <div className="px-4 py-5 bg-white rounded">
         <h2>Buy a Pass</h2>
         <hr />
-        <small className="text-uppercase mb-2 d-block">Step 1 of 4</small>
-        <Progress />
-        <Wizard>
-          <Step1 />
-          <Step2 />
+        <Wizard header={<WizardHeader />} footer={<WizardButtons />}>
+          <ProductSelect />
+          <AddOns />
+          <Details />
+          <Purchase />
         </Wizard>
       </div>
     </Col>
