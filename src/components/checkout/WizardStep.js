85494a9d1d383a682dc6e8c2d998e9c3bb38288c
Marge <marge.consunji@gmail.com>
2022-06-24 13:59:50 +0800

    Add bottom margin

--- src/components/checkout/WizardStep.js
+++ src/components/checkout/WizardStep.js
@@ -2,7 +2,7 @@ const WizardStep = ({ stepHeader, children }) => {
   return (
     <div>
       <h3>{stepHeader}</h3>
-      <div>{children}</div>
+      <div className="mb-5">{children}</div>
     </div>
   );
 };
