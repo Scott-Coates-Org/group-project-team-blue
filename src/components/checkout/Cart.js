88fc348cfdd2c361c9be0974d91e6724f63b4068
Marge <marge.consunji@gmail.com>
2022-06-24 14:04:16 +0800

    Adjust top and bottom padding

--- src/components/checkout/Cart.js
+++ src/components/checkout/Cart.js
@@ -3,7 +3,7 @@ import { Col } from 'reactstrap';
 const Cart = () => {
   return (
     <Col className="col-md-5">
-      <div className="bg-white px-4 py-5 rounded ">
+      <div className="bg-white px-4 pt-4 pb-5 rounded ">
         <h2>Your Cart</h2>
         <p>Your cart is currently empty.</p>
       </div>
--- src/components/checkout/WizardContainer.js
+++ src/components/checkout/WizardContainer.js
@@ -9,9 +9,9 @@ import Purchase from './steps/Purchase';
 
 const WizardContainer = () => {
   return (
-    <Col className="col-md-7 pb-2 pb-sm-0">
-      <div className="px-4 py-5 bg-white rounded">
-        <h2>Buy a Pass</h2>
+    <Col className="col-md-7 pb-2 pb-md-0">
+      <div className="px-4 pt-4 pb-5 bg-white rounded">
+        <h1>Buy a Pass</h1>
         <hr />
         <Wizard header={<WizardHeader />} footer={<WizardButtons />}>
           <ProductSelect />
