dfc7cbe59f6efd6ee5691fc92ed9bdd38a4ed01e
Marge <marge.consunji@gmail.com>
2022-06-24 18:35:09 +0800

    Add Logo component and button
    - Also vertically and horizontally center the logo and button

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,5 +1,17 @@
+import Logo from 'components/logo/Logo';
+import { Button } from 'reactstrap';
+
 const Home = () => {
-  return <div>Test home</div>;
+  return (
+    <div className="w-100 h-100 vh-100 d-flex justify-content-center align-items-center homepage-bg">
+      <div>
+        <Logo />
+        <div className="d-flex justify-content-center">
+          <div id="homepage-btn">Buy a Pass</div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
