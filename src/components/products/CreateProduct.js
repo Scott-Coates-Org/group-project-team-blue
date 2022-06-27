3741eae36fd678b1bc67a3a5b403abd508c38df5
prb01 <p.bergstroem@gmail.com>
2022-06-27 14:08:25 -0500

    added conditional fields & validation

--- src/components/products/CreateProduct.js
+++ src/components/products/CreateProduct.js
@@ -5,20 +5,63 @@ import { createProduct, savePhoto } from "redux/product";
 
 const CreateProduct = (props) => {
   const dispatch = useDispatch();
+
   const {
     register,
     handleSubmit,
     reset,
+    resetField,
+    getValues,
+    watch,
     formState: { errors },
-  } = useForm();
-  const { ref: titleRef, ...titleRest } = register("title", { required: true });
-  const { ref: descRef, ...descRest } = register("desc", { required: true });
-  const { ref: typeRef, ...typeRest } = register("type", { required: true });
-  const { ref: priceRef, ...priceRest } = register("price", { required: true, valueAsNumber: true,});
-  const { ref: photoRef, ...photoRest } = register("photo", { required: true });
-  const { ref: statusRef, ...statusRest } = register("status", { required: true });
-  const { ref: roomIdRef, ...roomIdRest } = register("room", { });
-  const { ref: durationRef, ...durationRest } = register("duration", { });
+  } = useForm({
+    mode: "onChange",
+    defaultValues: {
+      title: "",
+      desc: "",
+      type: "",
+      price: 0,
+      photo: "",
+      status: "",
+      room: null,
+      duration: null,
+    },
+  });
+
+  const msgIfEmpty = (name = "") => `${name} cannot be empty`;
+
+  const { ref: titleRef, ...titleRest } = register("title", {
+    required: msgIfEmpty("Title"),
+  });
+  const { ref: descRef, ...descRest } = register("desc", {
+    required: msgIfEmpty("Description"),
+  });
+  const { ref: typeRef, ...typeRest } = register("type", {
+    required: msgIfEmpty("Type"),
+  });
+  const { ref: priceRef, ...priceRest } = register("price", {
+    required: msgIfEmpty("Price"),
+    valueAsNumber: true,
+  });
+  const { ref: photoRef, ...photoRest } = register("photo", {
+    required: msgIfEmpty("Photo"),
+  });
+  const { ref: statusRef, ...statusRest } = register("status", {
+    required: msgIfEmpty("Status"),
+  });
+  const { ref: roomRef, ...roomRest } = register("room", {
+    validate: (v) => requiredIfProductType(v),
+  });
+  const { ref: durationRef, ...durationRest } = register("duration", {
+    validate: (v) => requiredIfProductType(v),
+    setValueAs: (v) => parseInt(v),
+  });
+
+  const requiredIfProductType = (v) => {
+    if (getValues("type") === "product" && (v == null || v === NaN || v.length === 0))
+      return msgIfEmpty();
+    return true;
+  };
 
   const onSubmit = (data) => {
     if (Object.keys(errors).length) {
@@ -35,6 +78,8 @@ const CreateProduct = (props) => {
               price: data.price,
               photo: photoUrl,
               status: data.status,
+              roomId: data.room,
+              duration: data.duration,
             })
           ).then(() => {
             reset();
@@ -48,43 +93,118 @@ const CreateProduct = (props) => {
   return (
     <Form onSubmit={handleSubmit(onSubmit)} className="p-3 my-3 border border-primary">
       <FormGroup>
-        <Label for="title">Product Title</Label>
-        <Input id="title" type="text" {...titleRest} innerRef={titleRef} invalid={errors.title} />
+        <Label for="title">
+          Title<span className="text-danger">*</span>
+        </Label>
+        <Input
+          id="title"
+          type="text"
+          {...titleRest}
+          innerRef={titleRef}
+          invalid={errors.title ? true : false}
+        />
       </FormGroup>
       <FormGroup>
-        <Label for="desc">Product Description</Label>
-        <Input id="desc" type="text" {...descRest} innerRef={descRef} invalid={errors.desc} />
+        <Label for="desc">
+          Description<span className="text-danger">*</span>
+        </Label>
+        <Input
+          id="desc"
+          type="text"
+          {...descRest}
+          innerRef={descRef}
+          invalid={errors.desc ? true : false}
+        />
       </FormGroup>
       <FormGroup>
-        <Label for="type">Product Type</Label>
-        <Input id="type" type="select" {...typeRest} innerRef={typeRef} invalid={errors.type}>
+        <Label for="type">
+          Type<span className="text-danger">*</span>
+        </Label>
+        <Input
+          id="type"
+          type="select"
+          {...typeRest}
+          innerRef={typeRef}
+          invalid={errors.type ? true : false}
+          onChange={(e) => {typeRest.onChange(e); resetField("room"); resetField("duration");}}
+        >
           <option value="" hidden></option>
           <option value="addon">Add-On</option>
+          <option value="product">Product</option>
         </Input>
       </FormGroup>
       <FormGroup>
-        <Label for="price">Product Price</Label>
-        <Input id="price" type="number" {...priceRest} innerRef={priceRef} invalid={errors.price} />
+        <Label for="room">
+          Room<span className="text-danger">*</span>
+        </Label>
+        <Input
+          id="room"
+          type="select"
+          {...roomRest}
+          innerRef={roomRef}
+          invalid={errors.room ? true : false}
+          disabled={watch("type") !== "product"}
+        >
+          <option value="" hidden></option>
+          <option value="Test">Test</option>
+          {/* Get all rooms to display as options. Value = room ID */}
+        </Input>
       </FormGroup>
       <FormGroup>
-        <Label for="photo">Product Photo</Label>
+        <Label for="duration">
+          Duration<span className="text-danger">*</span>
+        </Label>
+        <Input
+          id="duration"
+          type="select"
+          {...durationRest}
+          innerRef={durationRef}
+          invalid={errors.duration ? true : false}
+          disabled={watch("type") !== "product"}
+        >
+          <option value="" hidden></option>
+          <option value="0">All Day</option>
+          <option value="60">60min</option>
+          <option value="90">90min</option>
+          <option value="120">120min</option>
+          {/* Get all durations to display as options */}
+        </Input>
+      </FormGroup>
+      <FormGroup>
+        <Label for="price">
+          Price<span className="text-danger">*</span>
+        </Label>
+        <Input
+          id="price"
+          type="number"
+          {...priceRest}
+          innerRef={priceRef}
+          invalid={errors.price ? true : false}
+        />
+      </FormGroup>
+      <FormGroup>
+        <Label for="photo">
+          Photo<span className="text-danger">*</span>
+        </Label>
         <Input
           id="photo"
           type="file"
           accept="image/*"
           {...photoRest}
           innerRef={photoRef}
-          invalid={errors.photo}
+          invalid={errors.photo ? true : false}
         />
       </FormGroup>
       <FormGroup>
-        <Label for="status">Product Status</Label>
+        <Label for="status">
+          Status<span className="text-danger">*</span>
+        </Label>
         <Input
           id="status"
           type="select"
           {...statusRest}
           innerRef={statusRef}
-          invalid={errors.status}
+          invalid={errors.status ? true : false}
         >
           <option value="" hidden></option>
           <option value="active">Active</option>
