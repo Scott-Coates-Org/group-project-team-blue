1e55c87194eebe82bb715c593c17f486096040c3
Marge <marge.consunji@gmail.com>
2022-07-01 22:38:23 +0800

    Update Wizard
    - Remote footer prop, so instead of passing WizardButtons to each step, I've manually added the buttons for each step. Doing this to implement a sub-step (so to speak) for Step 1 (the date selector).
    - Update components

--- src/components/checkout/WizardContainer.js
+++ src/components/checkout/WizardContainer.js
@@ -1,23 +1,26 @@
+import { useState } from 'react';
 import { Col } from 'reactstrap';
 import { Wizard } from 'react-use-wizard';
 import WizardHeader from './WizardHeader';
 import WizardButtons from './WizardButtons';
-import ProductSelect from './steps/ProductSelect';
+import ProductSelect from './steps/ProductSelect/ProductSelect';
 import AddOns from './steps/AddOns';
-import Details from './steps/Details';
-import Purchase from './steps/Purchase';
+import ContactDetails from './steps/ContactDetails';
+import PaymentDetails from './steps/PaymentDetails';
 
 const WizardContainer = () => {
+  const [dateSelect, setDateSelect] = useState(true);
+
   return (
     <Col className="col-md-7 pb-2 pb-md-0">
       <div className="px-4 pt-4 pb-5 bg-white rounded">
         <h1>Buy a Pass</h1>
         <hr />
-        <Wizard header={<WizardHeader />} footer={<WizardButtons />}>
+        <Wizard header={<WizardHeader />}>
           <ProductSelect />
           <AddOns />
-          <Details />
-          <Purchase />
+          <ContactDetails />
+          <PaymentDetails />
         </Wizard>
       </div>
     </Col>
