2ed69b55a210f1d28210f2d5c1bd936dd2e3d4aa
Marge <marge.consunji@gmail.com>
2022-07-01 23:55:49 +0800

    Moved calendar/date picker to its own component

---
+++ src/components/checkout/steps/DateSelect/DateSelect.js
@@ -0,0 +1,27 @@
+import { useState } from 'react';
+import Calendar from 'react-calendar';
+import WizardStep from 'components/checkout/WizardStep';
+import 'react-calendar/dist/Calendar.css';
+import './dateSelect.css';
+
+const DateSelect = () => {
+  const [date, setDate] = useState(new Date());
+
+  const handleSetDate = () => {
+    setDate(date);
+    console.log(date);
+  };
+  return (
+    <WizardStep>
+      <Calendar
+        onChange={setDate}
+        value={date}
+        minDate={new Date()}
+        maxDate={new Date('December 31, 2025')}
+        onClickDay={handleSetDate}
+      />
+    </WizardStep>
+  );
+};
+
+export default DateSelect;
