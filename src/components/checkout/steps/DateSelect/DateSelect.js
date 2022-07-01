fc99e016ee59df8c316b99f838263cb6503f74b2
Marge <marge.consunji@gmail.com>
2022-07-02 00:59:40 +0800

    Disable continue button until date is selected
    - The buttons are back for a reason. Set button to disabled unless a date has been selected. A purchase can't move forward until a date is selected.

--- src/components/checkout/steps/DateSelect/DateSelect.js
+++ src/components/checkout/steps/DateSelect/DateSelect.js
@@ -1,3 +1,5 @@
+import { Button } from 'reactstrap';
+import { useWizard } from 'react-use-wizard';
 import { useState } from 'react';
 import Calendar from 'react-calendar';
 import WizardStep from 'components/checkout/WizardStep';
@@ -5,21 +7,29 @@ import 'react-calendar/dist/Calendar.css';
 import './dateSelect.css';
 
 const DateSelect = () => {
-  const [date, setDate] = useState(new Date());
+  const { nextStep } = useWizard();
+  const [date, setDate] = useState(null);
 
-  const handleSetDate = () => {
-    setDate(date);
-    console.log(date);
-  };
   return (
     <WizardStep stepHeader="Select a date">
-      <Calendar
-        onChange={setDate}
-        value={date}
-        minDate={new Date()}
-        maxDate={new Date('December 31, 2025')}
-        onClickDay={handleSetDate}
-      />
+      <div className="mb-4">
+        <Calendar
+          onChange={setDate}
+          value={date}
+          minDate={new Date()}
+          maxDate={new Date('December 31, 2025')}
+        />
+      </div>
+      <div className="d-flex">
+        <Button
+          color="warning"
+          onClick={() => nextStep()}
+          className="flex-grow-1 w-75"
+          disabled={!date}
+        >
+          Continue
+        </Button>
+      </div>
     </WizardStep>
   );
 };
