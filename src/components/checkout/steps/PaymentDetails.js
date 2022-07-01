75b95e9ebb2da214e3e4fbaa35ef573bb5fa8af8
Marge <marge.consunji@gmail.com>
2022-07-01 23:57:03 +0800

    Remove buttons

--- src/components/checkout/steps/PaymentDetails.js
+++ src/components/checkout/steps/PaymentDetails.js
@@ -1,24 +1,11 @@
-import { useWizard } from 'react-use-wizard';
-import { Button } from 'reactstrap';
 import WizardStep from '../WizardStep';
 
 const PaymentDetails = () => {
-  const { isLastStep, previousStep } = useWizard();
   return (
     <WizardStep stepHeader="Select payment details">
       <p>
         Stripe bit goes here. User should be able to complete their purchase.
       </p>
-      <div className="d-flex">
-        <Button
-          color="secondary"
-          onClick={() => previousStep()}
-          className={`flex-grow-1 w-50 ${isLastStep ? '' : 'mr-2'}`}
-          outline
-        >
-          Back
-        </Button>
-      </div>
     </WizardStep>
   );
 };
