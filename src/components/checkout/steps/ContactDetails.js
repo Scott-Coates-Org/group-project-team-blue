7f8820763719629975dbdc65b2336619a5ce9d89
Marge <marge.consunji@gmail.com>
2022-07-02 13:39:09 +0800

    Add step buttons back in

--- src/components/checkout/steps/ContactDetails.js
+++ src/components/checkout/steps/ContactDetails.js
@@ -1,9 +1,29 @@
+import { useWizard } from 'react-use-wizard';
+import { Button } from 'reactstrap';
 import WizardStep from '../WizardStep';
 
 const ContactDetails = () => {
+  const { nextStep, previousStep } = useWizard();
   return (
     <WizardStep stepHeader="Enter your details">
       <p>User inputs their contact details in this bit.</p>
+      <div className="d-flex">
+        <Button
+          color="secondary"
+          onClick={() => previousStep()}
+          className={'flex-grow-1 w-50 mr-2'}
+          outline
+        >
+          Back
+        </Button>
+        <Button
+          color="warning"
+          onClick={() => nextStep()}
+          className="flex-grow-1 w-75"
+        >
+          Continue
+        </Button>
+      </div>
     </WizardStep>
   );
 };
