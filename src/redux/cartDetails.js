bbdce718b17d0c056daa6f434c38d6788ff90eb5
Marge <marge.consunji@gmail.com>
2022-07-07 17:16:35 +0800

    Add remove from cart action

--- src/redux/cartDetails.js
+++ src/redux/cartDetails.js
@@ -4,19 +4,51 @@ const cartDetails = createSlice({
   name: 'cart',
   initialState: {
     bookingDate: '',
-    products: [],
-    addons: [],
+    products: [
+      {
+        id: 1,
+        title: '60-min Pass',
+        type: 'product',
+        room: null,
+        duration: 60,
+        price: 30,
+        photo:
+          'https://res.cloudinary.com/dxzcdb0pm/image/upload/v1656913968/samples/allday_wxg2gs.jpg',
+        quantity: 0,
+        desc: 'Access to all parts of the park for an hour.',
+      },
+      {
+        id: 2,
+        title: '60-min Pass',
+        type: 'product',
+        room: null,
+        duration: 60,
+        price: 30,
+        photo:
+          'https://res.cloudinary.com/dxzcdb0pm/image/upload/v1656913968/samples/allday_wxg2gs.jpg',
+        quantity: 0,
+        desc: 'Access to all parts of the park for an hour.',
+      },
+    ],
     totalCost: 0,
     contactDetails: [],
   },
   reducers: {
-    getCartDetails: (state) => [],
     setBookingDate: (state, action) => {
       state.bookingDate = action.payload;
+    },
+    // addToCart: (state, action) => {
+    //   state.products = action.payload;
+    //   console.log(state);
+    // },
+    removeFromCart: (state, action) => {
+      state.products = state.products.filter(
+        (product) => product.id !== action.payload
+      );
     },
   },
 });
 
 export const reducer = cartDetails.reducer;
 
-export const { setBookingDate } = cartDetails.actions;
+export const { setBookingDate, removeFromCart } = cartDetails.actions;
