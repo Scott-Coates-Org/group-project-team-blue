c6eb99fc445d291fe5ce8c71b38dd51df8c62fec
Marge <marge.consunji@gmail.com>
2022-07-08 17:39:20 +0800

    Add new store functions - add to cart, reduce
    - Adds a new item to cart, if it exists, increase product quantity by one
    - Reduce product quantity by clicking on the '-' button

--- src/redux/cartDetails.js
+++ src/redux/cartDetails.js
@@ -4,32 +4,7 @@ const cartDetails = createSlice({
   name: 'cart',
   initialState: {
     bookingDate: '',
-    products: [
-      {
-        id: 1,
-        title: '60-min Pass',
-        type: 'product',
-        room: null,
-        duration: 60,
-        price: 30,
-        photo:
-          'https://res.cloudinary.com/dxzcdb0pm/image/upload/v1656913968/samples/allday_wxg2gs.jpg',
-        quantity: 0,
-        desc: 'Access to all parts of the park for an hour.',
-      },
-      {
-        id: 2,
-        title: '60-min Pass',
-        type: 'product',
-        room: null,
-        duration: 60,
-        price: 30,
-        photo:
-          'https://res.cloudinary.com/dxzcdb0pm/image/upload/v1656913968/samples/allday_wxg2gs.jpg',
-        quantity: 0,
-        desc: 'Access to all parts of the park for an hour.',
-      },
-    ],
+    products: [],
     totalCost: 0,
     contactDetails: [],
   },
@@ -37,10 +12,33 @@ const cartDetails = createSlice({
     setBookingDate: (state, action) => {
       state.bookingDate = action.payload;
     },
-    // addToCart: (state, action) => {
-    //   state.products = action.payload;
-    //   console.log(state);
-    // },
+    addToCart: (state, action) => {
+      if (
+        state.products.find((product) => product.id === action.payload.id) ==
+        null
+      ) {
+        state.products.push({
+          ...action.payload,
+          quantity: action.payload.quantity + 1,
+        });
+      } else {
+        state.products = state.products.map((product) => {
+          if (product.id !== action.payload.id) {
+            return product;
+          } else {
+            return { ...product, quantity: product.quantity + 1 };
+          }
+        });
+      }
+    },
+    reduceQty: (state, action) => {
+      state.products = state.products.map((product) => {
+        if (product.id === action.payload) {
+          return { ...product, quantity: product.quantity - 1 };
+        }
+        return product;
+      });
+    },
     removeFromCart: (state, action) => {
       state.products = state.products.filter(
         (product) => product.id !== action.payload
@@ -51,4 +49,5 @@ const cartDetails = createSlice({
 
 export const reducer = cartDetails.reducer;
 
-export const { setBookingDate, removeFromCart } = cartDetails.actions;
+export const { setBookingDate, addToCart, reduceQty, removeFromCart } =
+  cartDetails.actions;
