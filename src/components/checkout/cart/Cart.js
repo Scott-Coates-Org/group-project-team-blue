dcc47cfb22dc799fc19282c2a4196697d5ced607
Marge <marge.consunji@gmail.com>
2022-07-08 21:02:12 +0800

    Fix formatting - add space

--- src/components/checkout/cart/Cart.js
+++ src/components/checkout/cart/Cart.js
@@ -79,6 +79,7 @@ const Cart = () => {
   useEffect(() => {
     dispatch(getCosts());
   }, [products]);
+
   return (
     <Col className="col-md-5">
       <div className="bg-white px-4 pt-4 pb-5 rounded ">
