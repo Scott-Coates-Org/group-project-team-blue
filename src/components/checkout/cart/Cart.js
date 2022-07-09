6d161b362855354896ab0118561c26071b3c9431
Marge Consunji <marge.consunji@gmail.com>
2022-07-10 03:07:48 +0800

    37 customer UI layout 100 (#59)
    * Add pic of socks
    
    * Move select add on/s step to its own folder
    
    - Renamed file so it'd be a little easier to understand what this component is for
    - Sorted out the UI
    
    * Reduce bottom padding
    
    * Make store for cart details
    
    - Manage cart details that get used across a number of components
    
    * Add cartDetails
    
    * Set booking date in cartDetails state
    
    * Show booking date
    
    * Consume redux store data
    
    - Show date when a date is selected from the first step of the checkout wizard
    - Remove continue button (redundant)
    - Show completely empty cart if no items have been added to cart
    
    * Add remove from cart action
    
    * Reflect product selection changes in cart
    
    * Add new store functions - add to cart, reduce
    
    - Adds a new item to cart, if it exists, increase product quantity by one
    - Reduce product quantity by clicking on the '-' button
    
    * Use cart actions when adding to cart or reducing product quantity by 1
    
    * Dispatch cartDetails actions
    
    - Add and reduce quantity of addons and reflect in cart
    
    * Pass product type to store
    
    * Display subtotal, total, and cost breakdown
    
    * Fix formatting - add space
    
    * Add costs to store state
    
    - Costs is an object with these properties - subtotal and total
    - getCosts calculates values for both properties
    
    * Make it possible to setBookingTime
    
    * Show cart details
    
    - Show quantity, price, and booking time (if applicable) for products
    - Show changes in real time
    - Show payment breakdown
    
    * Set time on click
    
    * Sum up headcount
    
    - This reflects the total number of products (not addons) purchased
    
    * Change booking time text
    
    * Remove console logs
    
    * Set booking times and pass extra data to store
    
    - Set booking times in cart
    - This changes if a customer clicks on another time slot
    - Passing room to store in case it's needed elsewhere
    
    * Modify reduceQty reducer
    
    - Removes item from cart if a customer uses the decrement button and reduces qty to 0

--- src/components/checkout/cart/Cart.js
+++ src/components/checkout/cart/Cart.js
@@ -1,57 +1,61 @@
-import { Col, List, Button } from 'reactstrap';
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { getCosts } from 'redux/cartDetails';
+import { removeFromCart } from 'redux/cartDetails';
+import { Col, List } from 'reactstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTrash } from '@fortawesome/free-solid-svg-icons';
 
-const dummyDetails = {
-  subtotal: 78.0,
-  transactionFee: 0.0,
-};
+const CartDetails = ({ products }) => {
+  const dispatch = useDispatch();
+  const bookingDate = useSelector(({ cartDetails }) => cartDetails.bookingDate);
 
-const dummyCart = [
-  {
-    product: 'Product 1',
-    quantity: 2,
-    price: 10.0,
-  },
-  {
-    product: 'Product 2',
-    quantity: 1,
-    price: 20.0,
-  },
-];
-
-const CartDetails = () => {
   return (
     <div>
       <span className="bg-primary px-2 py-1 d-inline-block w-auto mb-3 text-white rounded">
-        Dummy Date
+        {bookingDate}
       </span>
 
       <List type="unstyled">
-        {dummyCart.map(({ product, quantity, price }) => (
-          <li key={product} className="d-flex align-items-start">
-            <p>
-              {quantity}
-              <span className="mx-1">x</span>
-              {product}
-            </p>
-            <p className="ml-auto d-flex align-items-center">
-              ${(quantity * price).toFixed(2)}
-              <FontAwesomeIcon
-                role="button"
-                icon={faTrash}
-                className="text-danger ml-2"
-              />
-            </p>
-          </li>
-        ))}
+        {products.map(({ quantity, title, price, id, time, duration }) => {
+          let isAllDay = duration === 0 && time.length === 0;
+          return (
+            <li key={id} className="d-flex align-items-start">
+              <div className="mb-2">
+                <p className="mb-0">
+                  {quantity}
+                  <span className="mx-1">x</span>
+                  {title}
+                </p>
+                {duration > 0 ? (
+                  <small className="mt-0 d-inline-block text-muted">
+                    {!isAllDay && time.length > 0
+                      ? `Booking time: ${time}`
+                      : `Please select a booking time`}
+                  </small>
+                ) : null}
+              </div>
+              <p className="ml-auto d-flex align-items-center">
+                ${(quantity * price).toFixed(2)}
+                <FontAwesomeIcon
+                  role="button"
+                  icon={faTrash}
+                  className="text-danger ml-2"
+                  onClick={() => dispatch(removeFromCart(id))}
+                />
+              </p>
+            </li>
+          );
+        })}
       </List>
     </div>
   );
 };
 
-const PaymentDetails = ({ details }) => {
-  const { subtotal, transactionFee, tax } = details;
+const PaymentDetails = ({ costs }) => {
+  const { subtotal, total } = costs;
+  const transactionFee = 5.0;
+  const taxRate = 0.05;
   return (
     <div>
       <h3>Total payment required</h3>
@@ -66,20 +70,13 @@ const PaymentDetails = ({ details }) => {
         </li>
         <li className="d-flex justify-content-between">
           <span>Tax</span>
-          <span>${((transactionFee + subtotal) * 0.1).toFixed(2)}</span>
+          <span>${(subtotal * taxRate).toFixed(2)}</span>
         </li>
         <li className="d-flex justify-content-between">
           <span>
             <strong>Total (Inc. Tax)</strong>
           </span>
-          <span>
-            $
-            {(
-              subtotal +
-              transactionFee +
-              (transactionFee + subtotal) * 0.1
-            ).toFixed(2)}
-          </span>
+          <span>${total.toFixed(2)}</span>
         </li>
       </List>
     </div>
@@ -87,17 +84,28 @@ const PaymentDetails = ({ details }) => {
 };
 
 const Cart = () => {
+  const dispatch = useDispatch();
+  const products = useSelector(({ cartDetails }) => cartDetails.products);
+  const costs = useSelector(({ cartDetails }) => cartDetails.costs);
+
+  useEffect(() => {
+    dispatch(getCosts());
+  }, [products]);
+
   return (
     <Col className="col-md-5">
-      <div className="bg-white px-4 pt-4 pb-5 rounded ">
+      <div className="bg-white p-4 rounded ">
         <h2>Your Cart</h2>
         <hr />
-        <CartDetails />
-        <hr />
-        <PaymentDetails details={dummyDetails} />
-        <Button color="warning" block>
-          Continue
-        </Button>
+        {products.length === 0 ? (
+          <p>Your cart is currently empty.</p>
+        ) : (
+          <>
+            <CartDetails products={products} />
+            <hr />
+            <PaymentDetails costs={costs} />
+          </>
+        )}
       </div>
     </Col>
   );
