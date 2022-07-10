3e00b921638dd61d07bf00a5abd8c8c59488325f
giest95 <94708484+giest95@users.noreply.github.com>
2022-07-10 11:33:31 -0500

    36 stripe checkout 50 (#60)
    * the Stripe.js pulls a callable function
    
    * navigating to /stripe and filling the form allows for test payment completion
    
    Added stripe component with payment form
    
    stripe component is not currently tied to checkout wizard and will need configuring to do so (speak with marge)
    
    added customerdetails form that creates a stripe customer and adds them to the database. this couples with the customer redux for grabbing necessary information. This may need to be integrated into checkout wizard.
    
    added server functions and dependencies

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,12 @@
 // https://dev.to/thatgalnatalie/how-to-get-started-with-redux-toolkit-41e
-import { configureStore } from '@reduxjs/toolkit';
-import { combineReducers } from 'redux';
-import { reducer as user } from './user';
-import { reducer as widget } from './widget';
-import { reducer as product } from './product';
-import { reducer as room } from './room';
-import { reducer as opentime } from './opentime';
-import { reducer as cartDetails } from './cartDetails';
+import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers } from "redux";
+import { reducer as user } from "./user";
+import { reducer as widget } from "./widget";
+import { reducer as product } from "./product";
+import { reducer as room } from "./room";
+import { reducer as opentime } from "./opentime";
+import { reducer as customer } from "./customer";
 
 const reducer = combineReducers({
   user,
@@ -14,6 +14,7 @@ const reducer = combineReducers({
   product,
   room,
   opentime,
+  customer,
   cartDetails,
 });
 
