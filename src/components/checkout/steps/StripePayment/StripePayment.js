827d9c58e33f0c19b41f41e71346993269313488
Marge <marge.consunji@gmail.com>
2022-07-12 21:21:13 +0800

    Integrate Stripe component into checkout wizard

--- src/components/checkout/steps/StripePayment/StripePayment.js
+++ src/components/checkout/steps/StripePayment/StripePayment.js
@@ -1,14 +1,43 @@
 import { useWizard } from 'react-use-wizard';
+import { useState, useEffect } from 'react';
 import { Button } from 'reactstrap';
 import WizardStep from 'components/checkout/wizard-parts/WizardStep';
+import Stripe from 'components/stripe/Stripe';
+import { Elements } from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
+import { firebase } from 'firebase/client';
+require('firebase/functions');
+
+const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY);
 
 const StripePayment = () => {
   const { previousStep } = useWizard();
+  const [clientSecret, setClientSecret] = useState('');
+
+  useEffect(() => {
+    const createPaymentIntent = firebase
+      .functions()
+      .httpsCallable('createPaymentIntent');
+
+    createPaymentIntent().then((result) =>
+      setClientSecret(result.data.clientSecret)
+    );
+  }, []);
+
+  const options = {
+    // passing the client secret obtained from the server
+    clientSecret: clientSecret,
+    loader: 'always',
+  };
   return (
     <WizardStep stepHeader="Select payment details">
-      <p>
-        Stripe bit goes here. User should be able to complete their purchase.
-      </p>
+      <div>
+        {clientSecret && (
+          <Elements options={options} stripe={stripePromise}>
+            <Stripe props={clientSecret} />
+          </Elements>
+        )}
+      </div>
       <div className="d-flex">
         <Button
           color="secondary"
