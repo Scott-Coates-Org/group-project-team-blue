820625e46f3d0bda60a6c18a9f4f8742688a0eef
Marge <marge.consunji@gmail.com>
2022-07-12 15:52:46 +0800

    Make booking store for booking collection

---
+++ src/redux/booking.js
@@ -0,0 +1,88 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import firebaseClient from 'firebase/client';
+
+const initialState = {
+  data: [],
+  isLoaded: false,
+  hasErrors: false,
+};
+
+const booking = createSlice({
+  name: 'booking',
+  initialState,
+  reducers: {
+    getData: (state) => {},
+    getDataSuccess: (state, action) => {
+      state.isLoaded = true;
+      state.data = action.payload;
+    },
+    getDataFailure: (state, action) => {
+      state.isLoaded = true;
+      state.hasErrors = true;
+      state.errorMsg = action.payload;
+    },
+
+    createDataFailure: (state, action) => {
+      state.hasErrors = true;
+      state.errorMsg = action.payload;
+    },
+  },
+});
+
+export const reducer = booking.reducer;
+
+export const { getData, getDataSuccess, getDataFailure, createDataFailure } =
+  booking.actions;
+
+export const fetchAllBookings = createAsyncThunk(
+  'booking/fetchAllBookings',
+  async (_, thunkAPI) => {
+    thunkAPI.dispatch(getData());
+
+    try {
+      const data = await _fetchAllBookingsFromDb();
+      thunkAPI.dispatch(getDataSuccess(data));
+    } catch (error) {
+      console.error('error', error);
+      thunkAPI.dispatch(getDataFailure(error));
+    }
+  }
+);
+
+export const createBooking = createAsyncThunk(
+  'booking/createBooking',
+  async (payload, thunkAPI) => {
+    try {
+      await _createBooking(
+        payload.customer,
+        payload.order,
+        payload.stripe,
+        payload.waiver
+      );
+    } catch (error) {
+      console.error('error', error);
+      thunkAPI.dispatch(createDataFailure());
+    }
+  }
+);
+
+async function _fetchAllBookingsFromDb() {
+  const snapshot = await firebaseClient.firestore().collection('booking').get();
+
+  const bookingData = snapshot.docs.map((doc) => ({
+    id: doc.id,
+    ...doc.data(),
+  }));
+
+  return bookingdata;
+}
+
+async function _createBooking(customer, order, stripe, waiver) {
+  const doc = await firebaseClient.firestore().collection('booking').add({
+    customer,
+    order,
+    stripe,
+    waiver,
+  });
+  return doc;
+}
