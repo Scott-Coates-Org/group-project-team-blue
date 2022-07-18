2b56fa51b7d1b2ffedd909d724f33808a655cf0f
Marge <marge.consunji@gmail.com>
2022-07-18 18:13:24 +0800

    Make reducer to set participant list in store

--- src/redux/cartDetails.js
+++ src/redux/cartDetails.js
@@ -18,9 +18,13 @@ const cartDetails = createSlice({
       ipAddress: '',
       userAgent: ''
     },
+    participants: null,
     headcount: 0,
   },
   reducers: {
+    setParticipants: (state, action) => {
+      state.participants = action.payload.map(participant => participant);
+    },
     setContactDetails: (state, action) => {
       state.customerDetails = action.payload;
     },
@@ -101,6 +105,7 @@ const cartDetails = createSlice({
 export const reducer = cartDetails.reducer;
 
 export const {
+  setParticipants,
   setContactDetails,
   getHeadcount,
   getCosts,
