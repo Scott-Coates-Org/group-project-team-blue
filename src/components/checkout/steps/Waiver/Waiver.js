14845784e819460c70599c09f9372014e3b44c7a
Marge <marge.consunji@gmail.com>
2022-07-18 18:14:00 +0800

    Remove unused imports, move Continue button to form

--- src/components/checkout/steps/Waiver/Waiver.js
+++ src/components/checkout/steps/Waiver/Waiver.js
@@ -1,31 +1,11 @@
-import { useWizard } from "react-use-wizard";
-import { Button } from "reactstrap";
 import WizardStep from "components/checkout/wizard-parts/WizardStep";
 import Participants from "../Participants/Participants";
 
 const Waiver = () => {
-  const { nextStep, previousStep } = useWizard();
   return (
-    <WizardStep stepHeader="Sign the Waiver">
-      <p>Customer signs a waiver before moving forward with their purchase</p>
+    <WizardStep stepHeader="Add Event Participants">
+      <p>All participants must sign a digital waiver before entering the park. Add each participants name below and you'll get links to individual digital waivers emailed to you upon checkout.</p>
       <Participants />
-      <div className="d-flex">
-        <Button
-          color="secondary"
-          onClick={() => previousStep()}
-          className={"flex-grow-1 w-50 mr-2"}
-          outline
-        >
-          Back
-        </Button>
-        <Button
-          color="warning"
-          onClick={() => nextStep()}
-          className="flex-grow-1 w-75"
-        >
-          Continue
-        </Button>
-      </div>
     </WizardStep>
   );
 };
