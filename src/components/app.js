252921db151b644a1baff537240ac45fee2f49f8
ksitkyaw <ksithukyaw26@gmail.com>
2022-07-19 12:56:50 +0630

    remove two test routes from app.js

--- src/components/app.js
+++ src/components/app.js
@@ -14,11 +14,8 @@ import store from "redux/store";
 import { getData, getDataSuccess } from "redux/user";
 import ErrorBoundary from "components/error-boundary";
 import Dashboard from "./dashboard/Dashboard";
-import CreateProduct from "./products/CreateProduct";
-import ProductList from "./products/ProductList";
 import SendGrid from "./sendgrid/SendGrid";
 import WaiverForm from "./waiver/WaiverForm";
-import BookingDetails from "./AllBooking/BookingDetails";
 
 // DO NOT import BrowserRouter (as per tutorial). that caused router to not actually do anything.
 // see here: https://stackoverflow.com/questions/63554233/react-router-v5-history-push-changes-the-address-bar-but-does-not-change-the
@@ -59,9 +56,6 @@ function App() {
       <AuthProvider onLogin={storeUserData}>
         <Router history={history}>
           <Switch>
-          <Route path="/bookings/:id">
-              <BookingDetails />
-            </Route>
             <Route
               path="/login"
               render={(routeProps) => (
@@ -75,11 +69,6 @@ function App() {
               )}
             />
             <Route exact path="/" render={() => <Home />} />
-            <Route
-              exact
-              path="/createproduct"
-              render={() => <CreateProduct />}
-            />
             <Route path="/checkout" render={() => <Checkout />} />
             <Route path="/sendgrid" render={() => <SendGrid />} />
             <Route path="/waiver/:bookingId/:waiverId" render={() => <WaiverForm />} />
