b62a68474b3e1af79196b88874e0f40a2c1adb45
Marge <marge.consunji@gmail.com>
2022-07-19 12:38:57 +0800

    Clean up imports remove unused import

--- src/components/checkout/steps/DateSelect/DateSelect.js
+++ src/components/checkout/steps/DateSelect/DateSelect.js
@@ -1,5 +1,5 @@
 import { Button } from 'reactstrap';
-import { useSelector, useDispatch } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { setBookingDate } from 'redux/cartDetails';
 import { useWizard } from 'react-use-wizard';
 import { useState, useEffect } from 'react';
