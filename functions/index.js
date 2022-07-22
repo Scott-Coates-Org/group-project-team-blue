cedbb6dd5a6cfa5e5bbd2707234222344899d47c
Marge Consunji <marge.consunji@gmail.com>
2022-07-22 14:38:22 +0800

    Send email on success (#114)
    * Update ecmaVersion
    
    * Send email through Sendgrid - test
    
    * Add order details
    
    - Format total amount
    - Shorten confirmation date
    - Show products ordered
    
    * Don't show waiver if participants arr empty
    
    * Git ignore firebase debug logs
    
    * Fix booking time display

--- functions/index.js
+++ functions/index.js
@@ -85,9 +85,9 @@ exports.createPaymentIntent = functions.https.onCall(async (data, context) => {
   const orderProducts = removeUnnecessaryProductDetails(data.order.products);
   data.order.products = orderProducts;
 
-  const customer = JSON.stringify(data.customer);
-  const order = JSON.stringify(data.order);
-  const participants = JSON.stringify(data.participants);
+  // const customer = JSON.stringify(data.customer);
+  // const order = JSON.stringify(data.order);
+  // const participants = JSON.stringify(data.participants);
 
   const paymentIntent = await stripe.paymentIntents.create({
     amount: calculateOrderAmount(products),
@@ -168,9 +168,11 @@ exports.sendEmail = functions.https.onCall(async (data, context) => {
 
 const createWaiversInDB = async (bookingId) => {
   try {
+    // eslint-disable-next-line max-len
     const snapshot = await admin.firestore().collection("bookings").doc(bookingId).get();
 
-    const bookingData = snapshot ? { docID: snapshot.id, ...snapshot.data() } : null;
+    // eslint-disable-next-line max-len
+    const bookingData = snapshot ? {docID: snapshot.id, ...snapshot.data()} : null;
     const participants = bookingData.participants;
     const updatedParticipants = [];
 
@@ -184,16 +186,16 @@ const createWaiversInDB = async (bookingId) => {
 
       updatedParticipants.push({
         waiverId: docRef.id,
-        fullName: waiver.fullName
+        fullName: waiver.fullName,
       });
     }
 
     return updatedParticipants;
   } catch (error) {
+    // eslint-disable-next-line no-undef
     return response.status(400).end();
   }
-  
-}
+};
 
 const updateBookingWithWaiversInDB = async (bookingId, updatedParticipants) => {
   try {
@@ -201,9 +203,156 @@ const updateBookingWithWaiversInDB = async (bookingId, updatedParticipants) => {
       participants: updatedParticipants,
     });
   } catch (error) {
+    // eslint-disable-next-line no-undef
     return response.status(400).end();
   }
-}
+};
+
+const sendConfirmationEmail = async (docID) => {
+  // eslint-disable-next-line max-len
+  const snapshot = await admin.firestore().collection("bookings").doc(docID).get();
+
+  const orderDetails = snapshot.data();
+  console.log("orderdetails", orderDetails);
+
+  const {email, first, last} = orderDetails.customer;
+  const {bookingDate, products} = orderDetails.order;
+  const {amount, confirmDate, transactionID} = orderDetails.stripe;
+  const participants = orderDetails.participants;
+  const formattedConfirmDate = confirmDate.split("GMT+0000")[0];
+  const formattedAmount = (amount / 100).toFixed(2);
+
+  const baseUri = "https://team-blue-8951b.web.app/waiver";
+
+  const waiverList = participants?.map(({fullName, waiverId}) => (`
+  <li>${fullName} -
+  <a href="${baseUri}/docId/waiverId">
+  ${baseUri}/${docID}/${waiverId}</a></li>`)).join("");
+
+  const waiverSection = participants.length ? (`<div>
+  <h3>Waiver Forms</h3>
+  <p>In case you're jumping with 
+  other people, here's a list of
+  waiver form links to send to everyone in your party.</p>
+  <ol>
+  ${waiverList}
+  </ol>
+  </div>`) : "";
+
+  const productList = products.map(({title,
+    price, quantity, time, timeSlot}) => {
+    if (time) {
+      return (
+        `<tr>
+        <td style="padding: 10px; border-bottom: 1px solid gainsboro;">
+          <p style="margin-bottom: 3px;">${title}</p>
+            <small style="color: grey; 
+            font-size: 13px">Booking Time: ${time}</small>
+        </td>
+        <td style="text-align: center; 
+        border-bottom: 1px solid gainsboro;">
+        ${quantity}</td>
+        <td style="text-align: center; 
+        border-bottom: 1px solid gainsboro;">
+        ${price}</td>
+      </tr>`
+      );
+    } else {
+      return (
+        `<tr>
+        <td style="padding: 10px; border-bottom: 1px solid gainsboro;">
+          <p style="margin-bottom: 3px;">${title}</p>
+        </td>
+        <td style="text-align: center; 
+        border-bottom: 1px solid gainsboro;">${quantity}</td>
+        <td style="text-align: center; 
+        border-bottom: 1px solid gainsboro;">${price}</td>
+      </tr>`
+      );
+    }
+  }).join("");
+
+  const msgBody = `<html>
+  <head>
+    <title></title>
+  </head>
+  <body>
+  <div style="max-width: 550px;
+  margin: 0 auto;
+  font-family: 'Arial', sans-serif;
+  color:black;
+  font-size:14px;
+  line-height:20px; padding:16px 16px 16px 16px;">
+          <div style="margin-bottom: 50px">
+              <h2>Hi there, ${first}!</h2>
+              <p>Thank you for booking an event with us at Hopper.</p>
+              <p>If you're jumping as a group, 
+              we recommend you have everyone 
+              sign their waiver forms ahead of time.
+              You'll find individual links below for each waiver form.</p>
+          </div>
+
+          <h2>Your Booking</h2>
+          <div style="background-color: #F5F5F5; padding: 4px;">
+              <ul style="list-style: none;
+              margin-left: 0; padding-left: 10px; width: 100%;">
+                  <li><b>Billed To:</b>${first} ${last}
+                  </li>
+                  <li><b>Receipt No.</b> ${transactionID}</li>
+                  <li><b>Confirmation Date:</b> ${formattedConfirmDate}</li>
+                  <li><b>Order Total:</b> $${formattedAmount}</li>
+              </ul>
+          </div>
+          <div style="margin-bottom: 10px;">
+            <h3>Your Booking Details</h3>
+            <p><b>Booking Date: ${bookingDate}</b></p>
+          </div>
+              <table style="width: 100%; border-collapse: collapse;">
+              <tbody>
+                  <tr>
+                      <th style="text-align: left;
+                      padding: 10px;
+                      border-bottom: 1px solid gainsboro">
+                      Product
+                      </th>
+                      <th style="padding: 10px; border-bottom: 
+                      1px solid gainsboro">
+                      Quantity
+                      </th>
+                      <th style="padding: 10px;  border-bottom: 
+                      1px solid gainsboro">
+                      Price
+                      </th>
+                  </tr>
+                  ${productList}
+              </tbody>
+          </table>
+          <div style="text-align: right;">
+          <p style="font-size: 19px;"><b>Total</b>: $${formattedAmount}</p>
+          </div>
+          ${waiverSection}
+      </div>
+  </body>
+</html>`;
+
+  const msg = {
+    to: `${email}`,
+    from: "mentorshipteamblue@gmail.com",
+    subject: `Hopper - Booking Confirmation for ${first} ${last}`,
+    text: "Booking Confirmation Details",
+    html: msgBody,
+  };
+
+  sgMail
+      .send(msg)
+      .then(() => {}, (error) => {
+        console.error(error);
+
+        if (error.response) {
+          console.error(error.response.body);
+        }
+      });
+};
 
 exports.stripeConfirmAddToDB = functions.database
     .ref("/events/{eventId}")
@@ -238,13 +387,15 @@ exports.stripeConfirmAddToDB = functions.database
         },
         status: {
           type: "SUCCESS",
-          text: ""
-        }
+          text: "",
+        },
       });
 
       const updatedParticipants = await createWaiversInDB(docID);
 
       await updateBookingWithWaiversInDB(docID, updatedParticipants);
+
+      await sendConfirmationEmail(docID);
 
       return console.log({
         eventId: context.params.eventId,
