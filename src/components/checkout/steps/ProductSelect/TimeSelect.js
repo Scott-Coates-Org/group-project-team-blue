08986556d8ed70d9875f08018e88a882608266fc
Marge <marge.consunji@gmail.com>
2022-07-25 12:55:38 +0800

    Get product/room capacity

--- src/components/checkout/steps/ProductSelect/TimeSelect.js
+++ src/components/checkout/steps/ProductSelect/TimeSelect.js
@@ -24,11 +24,11 @@ const TimeSelect = ({ duration, id, timeSlots, handleSetTime }) => {
   const determineAvailability = (duration, time, times) => {
     switch (true) {
       case (duration === 120):
-        return time.remainingCapacity === 0 || times[time.idx + 1]?.remainingCapacity === 0 || times[time.idx + 2]?.remainingCapacity === 0 || times[time.idx + 3]?.remainingCapacity === 0 ? true : false;
+        return time.remainingCapacity <= 0 || times[time.idx + 1]?.remainingCapacity <= 0 || times[time.idx + 2]?.remainingCapacity <= 0 || times[time.idx + 3]?.remainingCapacity <= 0 ? true : false;
       case (duration === 90):
-        return time.remainingCapacity === 0 || times[time.idx + 1]?.remainingCapacity === 0 || times[time.idx + 2]?.remainingCapacity === 0 ? true : false;
+        return time.remainingCapacity <= 0  || times[time.idx + 1]?.remainingCapacity <= 0 || times[time.idx + 2]?.remainingCapacity <= 0 ? true : false;
       case (duration === 60):
-        return time.remainingCapacity === 0 || times[time.idx + 1]?.remainingCapacity === 0 ? true : false;
+        return time.remainingCapacity <= 0 || times[time.idx + 1]?.remainingCapacity <= 0 ? true : false;
     }
   };
 
